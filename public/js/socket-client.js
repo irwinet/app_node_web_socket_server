7cf6a09327f2304ec2ee6390107e99b4eeb1822f
Irwin <irwinestradatorres@gmail.com>
2021-04-04 16:51:03 -0500

    Enviar mensaje del Servidor a los Clientes

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -7,16 +7,20 @@ const btnEnviar = document.querySelector('#btnEnviar');
 const socket = io();
 
 socket.on('connect', () => {
-    console.log('Conectado');
+    // console.log('Conectado');
     lblOffline.style.display = 'none';
     lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-    console.log('Desconectado del servidor');
+    // console.log('Desconectado del servidor');
 
     lblOnline.style.display = 'none';
     lblOffline.style.display = '';
+});
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log(payload);
 });
 
 btnEnviar.addEventListener('click', () => {
